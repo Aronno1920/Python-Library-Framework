9f030b6cf083fb15a625a35323e517ec5eefa91e
Selim Ahmed <aronno1920@hotmail.com>
2025-07-01 23:36:05 +0600

    Update 1

---
+++ 2.create-numpy-arrays.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+list_array=np.array([1,2,3,4,5,6,7,8,9])
+print("array from list: ", list_array)
+
+zeros_array=np.zeros(5)
+print("array of zeros: ", zeros_array)
+
+ones_array=np.ones(5)
+print("array of zeros: ", ones_array)
+
+range_array = np.arange(1,10,2) #(start, end, step)
+print("array of ranges: ", range_array)
