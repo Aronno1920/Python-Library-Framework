ad50da388025f1b6803a4b42f47196edd72bdc3a
Selim Ahmed <aronno1920@hotmail.com>
2025-08-17 16:56:29 +0600

    basic controls

---
+++ Streamlit/controls.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.title("This is a title")
+st.header("This is a header")
+
+st.subheader("This is a subheader")
+
+st.markdown("This is a marksdown")
+
+st.code("This is a Codeblock. int a = 10")
+
+st.text("This is a text")
+
+st.write("This is a write")
